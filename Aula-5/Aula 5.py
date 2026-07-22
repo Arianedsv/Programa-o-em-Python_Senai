2b35b864375fffdcf4f580b4ab881f2c0a993ed6
Arianedsv <adsv09@gmail.com>
2026-07-22 20:42:27 -0300

    Add files via upload

---
+++ Aula-5/Aula 5.py
@@ -0,0 +1,73 @@
+#1
+#  frase = "Boas vindas"
+
+# print(frase)
+
+#2
+# booleana=True
+# print(type(booleana))
+
+#3
+# numero1=5.5
+# numero2=2.3
+
+# resultado=numero1*numero2
+
+# print(resultado)
+
+
+#4
+# numero1=420
+# numero2=12
+
+# resultado=numero1/numero2
+
+# print(resultado)
+
+#5
+# numero1=800
+# numero2=100
+
+# resultado=numero1-numero2
+
+# print(resultado)
+
+#6
+# numero1=25
+# numero2=4
+
+# resultado=numero1//numero2
+
+# print(resultado)
+
+#7
+# numero1=4.5
+# numero2=12.5
+# numero3=2.5
+# numero4=2.5
+
+# resultado=numero1*numero2*numero3*numero4
+
+# print(resultado)
+
+#8
+# numero=10
+
+# resultado=numero*2
+
+# print(resultado)
+
+#9
+# nome=input("Digite seu nome")
+# email=input("Digite seu email")
+# idade=input("Digite sua idade")
+# cidade=input("Digite sua cidade")
+# graduação=input("Digite dua graduação")
+# estado_civil=input("Digite seu estado civil")
+
+# print(nome)
+# print(email)
+# print(idade)
+# print(cidade)
+# print(graduação)
+# print(estado_civil)
